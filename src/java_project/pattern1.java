9a27e08386b86807b79ea031b6552712cb33cb5b
welcome <welcome@DESKTOP-DO7DGL8>
2019-06-20 08:36:35 +0530

    Commit

---
+++ src/java_project/pattern1.java
@@ -0,0 +1,19 @@
+package java_project;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		int row=4,os=3,is=;
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<os;j++)
+			{
+				System.out.println("");
+			}
+			System.out.println("*");
+			for(int j=0;j<)
+				
+			}
+		}
+	}
+
+}
---
+++ src/java_project/patternspgm.java
@@ -0,0 +1,24 @@
+package java_project;
+
+public class patternspgm {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=4; i++) {
+			
+			for(int j=0;j<4-1;j++)
+			{
+				System.out.print("");
+			}
+	for(int k=1; k!=(2*i-1);k++)
+		{
+		System.out.print("*");
+		
+		}
+	System.out.println();
+		}
+		
+
+	}
+
+}
